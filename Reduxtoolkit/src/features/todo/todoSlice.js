02072fa4f5365d91c27437b8ae353c9583fb7dd9
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-06-21 17:36:29 +0530

    used local storage to store and get the values with redux

--- Reduxtoolkit/src/features/todo/todoSlice.js
+++ Reduxtoolkit/src/features/todo/todoSlice.js
@@ -1,8 +1,11 @@
 import { createSlice,nanoid } from "@reduxjs/toolkit";
 
-const initialState={
-    todos: []
-}
+const localTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
+const initialState = {
+    todos: localTodos,
+};
+
 
 export const todoSlice= createSlice({
     name: 'todo',
@@ -15,24 +18,33 @@ export const todoSlice= createSlice({
                 completed:false,
             }
             state.todos.push(todo)
+            localStorage.setItem("todos", JSON.stringify(state.todos));
+
         },
         removeTodo: (state, action)=>{
             
             state.todos= state.todos.filter((todo)=> todo.id !== action.payload)
+            localStorage.setItem("todos", JSON.stringify(state.todos));
 
         },
 
         updateTodo: (state, action)=>{
    
            state.todos= state.todos.map((todo)=>(todo.id===action.payload.id? action.payload :todo))
+           localStorage.setItem("todos", JSON.stringify(state.todos));
+
         },
 
         toggleTodo:(state, action)=>{
           
            state.todos= state.todos.map((todo)=>todo.id===action.payload? {...todo, completed:!todo.completed}:todo)
+           localStorage.setItem("todos", JSON.stringify(state.todos));
+
         }
 
-    }
+    },
+   
+      
 
 })
 export const {addTodo, removeTodo,updateTodo, toggleTodo}= todoSlice.actions
